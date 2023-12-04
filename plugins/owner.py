d71cf17864e9226a5ade723dca20238fefea49e2
Das{08} <125719200+Debayan08@users.noreply.github.com>
2023-12-04 16:45:05 +0530

    Update owner.py

--- plugins/owner.py
+++ plugins/owner.py
@@ -2,4 +2,4 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command("owner"))
 async def owner(client, message):
-    await message.reply_text("No.of 📞: +9832361550 \n 👤: @Das_2005_08")
+    await message.reply_text("🌟 Owner Details 🌟\nName: @Das_2005_08 \nAge: 18 🎂  \nGitHub: https://github.com/Debayan08 🐙  \nLanguage: Python 🐍  \nEmail: debayanabae2005@gmail.com 📧  \nInstagram 📸: das_abae\n The mind is everything. What you think, you become. - Mahabharata🌌✨")
