99c02285b39f10a332a90e64f2241be1327c6e72
Das{08} <125719200+Debayan08@users.noreply.github.com>
2023-12-21 00:52:23 +0530

    Update event.py
    it's working now

--- plugins/event.py
+++ plugins/event.py
@@ -1,40 +1,67 @@
-from pyrogram import Client, filters ,types
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup , InlineKeyboardButton
+from pyrogram.types import Message
 from pyrogram.types import CallbackQuery
 
-event = {"channel_id": "user", "event": "off"}
+  
 
-@Client.on_callback_query()
+event = {"-1152935968": "user", "event": "on"}
+
+@Client.on_callback_query(filters.regex('event_(.*)'))
 async def switch (client ,callback_query: CallbackQuery):
     chat_id = callback_query.message.chat.id 
+    path ="/home/das/Downloads/bot-main/plugins/video.mp4"
+    help_list = "🤖 MIKU-BOT Commands 💬\n- /anime - 🎌 Get anime details\n- /bored - 🕰️ Find activities to cure boredom \n- /jokes - 😄 Enjoy funny jokes\n- /rps - ✊✋✌️ Play Rock, Paper, Scissors\n- /waifu - ❤️ Find your waifu\n- /fact - 🧠 Discover interesting facts\n- /qr - 📱 Generate QR codes \n- /owner 🧑🏻‍💻- Contact of Owner"
     query_id_ = callback_query.id 
+    input = callback_query.data.split("_", 1)[1]
     event_on = "EVENT ON 📢"
     event_off = "EVENT OFF 📢"
-
-    if callback_query.data == "on":
-       event["channel_id"] = chat_id
-       event["event"] = "on"
-       await client.answer_callback_query(callback_query_id= query_id_,text=event_on, show_alert=True)
-    else:
+    if input == "on":
         event["channel_id"] = chat_id
-        event["event"] = "off"
-        await client.answer_callback_query(callback_query_id= query_id_,text=event_off, show_alert=True)
+        event["event"] = "on"
+        await client.answer_callback_query(callback_query_id= query_id_,text=event_on, show_alert=True)
+    elif input == "help":
+        await client.send_video(chat_id,video = path, caption = help_list)
+    else:
+         event["channel_id"] = chat_id
+         event["event"] = "off"
+         await client.answer_callback_query(callback_query_id= query_id_,text=event_off, show_alert=True)
 
 @Client.on_message(filters.command("event"))
-async def eve (client,message):
+async def eve (client, message):
     ms_g = 'Do you want to turn the event 🎐'
     keybord=InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("OFF", callback_data="off")],
-            [InlineKeyboardButton("ON", callback_data="on")]
+            [InlineKeyboardButton("OFF", callback_data="event_off")],
+            [InlineKeyboardButton("ON", callback_data="event_on")]
         ])
     await message.reply(text = ms_g , reply_markup = keybord)
 
-@Client.on_chat_member_updated(filters.new_chat_members)
-async def new_member(_, message):
-    global event
-    if event == True:
-     new_m = message.from_user.username
-     await message.reply(f'{new_m} Welcome to the group 🎐')
-    else:
-       return
+#https://docs.pyrogram.org/api/filters#module-pyrogram.filters
+@Client.on_message(filters.left_chat_member)
+async def new_member(client: Client, message: Message):
+     user_name = message.from_user.username
+     chat_titel = message.chat.title
+     await message.reply(f'@{user_name} left the {chat_titel} 🥲')
+
+     
+@Client.on_message(filters.new_chat_members)
+async def new_member(client: Client, message: Message):
+     user_name = message.from_user.username
+     chat_titel = message.chat.title
+     btn =InlineKeyboardMarkup(
+    [
+        [InlineKeyboardButton("Help-list", callback_data="event_help")]
+    ])
+     await message.reply(f'Welcome @{user_name} to {chat_titel} 🎉', reply_markup = btn)
+
+
+
+     
+
+
+
+
+
+
+
